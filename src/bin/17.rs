d53441d6645963a56250523fb02bb93fa03cd196
Ludi Rehak <ludi317@gmail.com>
2023-12-21 19:38:08 -0800

    Day 17. My first tango with Dijkstra. If you can understand a minheap, you can understand Dijkstra. In a nutshell, start with the source at cost 0. Use min heap that stores the state (node number, position, or more) and its cost. Pop off the smallest cost, and add its neighbors to the heap IF the neighbor hasn't been seen before or its cost is a new low (tracked in a hashmap). Nodes are popped off in order of increasing cost path (from smallest -> highest) from source node.

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -3,45 +3,117 @@ use pheap::PairingHeap;
 use std::usize;
 advent_of_code::solution!(17);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let graph: Vec<Vec<usize>> = vec![
-        vec![0, 4, 0, 0, 0, 0, 0, 8, 0],
-        vec![4, 0, 8, 0, 0, 0, 0, 11, 0],
-        vec![0, 8, 0, 7, 0, 4, 0, 0, 2],
-        vec![0, 0, 7, 0, 9, 14, 0, 0, 0],
-        vec![0, 0, 0, 9, 0, 10, 0, 0, 0],
-        vec![0, 0, 4, 14, 10, 0, 2, 0, 0],
-        vec![0, 0, 0, 0, 0, 2, 0, 1, 6],
-        vec![8, 11, 0, 0, 0, 0, 1, 0, 7],
-        vec![0, 0, 2, 0, 0, 0, 6, 7, 0],
-    ];
+// const UP: usize = 0;
+const RIGHT: usize = 1;
+const DOWN: usize = 2;
+// const LEFT: usize = 3;
 
-    let mut dists: HashMap<usize, usize> = HashMap::new();
-    let mut frontier: PairingHeap<usize, usize> = PairingHeap::new();
-    frontier.insert(0, 0);
-    dists.insert(0, 0);
+const DIRS: [[isize; 2]; 4] = [[-1, 0], [0, 1], [1, 0], [0, -1]];
 
-    while let Some((main_n, _)) = frontier.delete_min() {
-        for (n, &d) in graph[main_n].iter().enumerate() {
-            if d == 0 {continue}
-            if !dists.contains_key(&n) || dists[&n] > dists[&main_n] + d {
-                frontier.insert(n, dists[&main_n] + d);
-                dists.insert(n, dists[&main_n] + d);
+// each state in the graph
+#[derive(Copy, Clone, Eq, PartialEq, Hash)]
+struct State {
+    pos: [isize; 2],
+    dir: usize,
+    streak: usize,
+}
+
+pub fn part_one(input: &str) -> Option<isize> {
+    let grid: Vec<Vec<isize>> = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| c.to_digit(10).unwrap() as isize)
+                .collect()
+        })
+        .collect();
+
+    dijkstra(&grid, 1, 3)
+}
+
+fn neighbors(o: &State, min_streak: usize, max_streak: usize) -> Vec<State> {
+    let mut neighbors: Vec<State> = vec![];
+    let new_pos = |dir: usize| [o.pos[0] + DIRS[dir][0], o.pos[1] + DIRS[dir][1]];
+    // straight
+    if o.streak < max_streak {
+        neighbors.push(State {
+            pos: new_pos(o.dir),
+            dir: o.dir,
+            streak: o.streak + 1,
+        })
+    }
+    if o.streak >= min_streak {
+        // left
+        let left = (o.dir + 3) % 4;
+        neighbors.push(State {
+            pos: new_pos(left),
+            dir: left,
+            streak: 1,
+        });
+        // right
+        let right = (o.dir + 1) % 4;
+        neighbors.push(State {
+            pos: new_pos(right),
+            dir: right,
+            streak: 1,
+        })
+    }
+    neighbors
+}
+
+fn dijkstra(grid: &Vec<Vec<isize>>, min_streak: usize, max_streak: usize) -> Option<isize> {
+    let rows = grid.len() as isize;
+    let cols = grid[0].len() as isize;
+
+    let mut frontier: PairingHeap<State, isize> = PairingHeap::new();
+    let mut dists: HashMap<State, isize> = HashMap::new();
+    for start in [
+        State {
+            pos: [0, 0],
+            dir: RIGHT,
+            streak: 1,
+        },
+        State {
+            pos: [0, 0],
+            dir: DOWN,
+            streak: 1,
+        },
+    ] {
+        frontier.insert(start, 0);
+        dists.insert(start, 0);
+    }
+
+    while let Some((orig_state, cost)) = frontier.delete_min() {
+        if orig_state.pos == [rows - 1, cols - 1] && orig_state.streak >= min_streak {
+            return Some(cost);
+        }
+
+        for n in neighbors(&orig_state, min_streak, max_streak) {
+            if n.pos[0] < 0 || n.pos[0] >= rows || n.pos[1] < 0 || n.pos[1] >= cols {
+                continue;
+            }
+            let next_cost = cost + grid[n.pos[0] as usize][n.pos[1] as usize];
+            if !dists.contains_key(&n) || dists[&n] > next_cost {
+                dists.insert(n, next_cost);
+                frontier.insert(n, next_cost);
             }
         }
     }
 
-
-
-    let mut items: Vec<_> = dists.iter().collect();
-    items.sort_by_key(|&(_, v)| v);
-    println!("{:?}", items);
-
     None
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<isize> {
+    let grid: Vec<Vec<isize>> = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| c.to_digit(10).unwrap() as isize)
+                .collect()
+        })
+        .collect();
+
+    dijkstra(&grid, 4, 10)
 }
 
 #[cfg(test)]
@@ -51,12 +123,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(102));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(94));
     }
 }
